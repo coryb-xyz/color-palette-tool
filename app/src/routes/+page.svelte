8202fdf1f3ae3f868d880a615b98b3c43b65fafc
Cory B <contact@coryb.xyz>
2022-10-03 12:07:54 -0700

    added a div to make the dropzone

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -3,3 +3,15 @@
 </script>
 
 
+<div class="dropzone" id="dropzone">👀</div>
+
+
+<style>
+    .dropzone {
+        width: 10vw;
+        height: 10vh;
+        border-color: rgba(96, 96, 96, 255);
+        border-style: dashed;
+        font-size: 5vh;
+    }
+</style>
