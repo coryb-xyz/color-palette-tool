f4e26a914926cda998c93cfb0ae5e308a85a8de0
Cory B <contact@coryb.xyz>
2022-10-05 16:21:40 -0700

    basic functionality implemented

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script>
     import PaletteWidget from "../components/PaletteWidget.svelte";
+    let widgetTracker = [0];
 </script>
 
 <body>
-    <PaletteWidget />
+    {#each widgetTracker as _}
+        <PaletteWidget/>
+    {/each}
+    <button on:click={() => widgetTracker = [...widgetTracker, 0]}>+</button>
 </body>
 
 <style>
     body {
         font-family: 'Open Sans', sans-serif;
+        display: grid;
+        gap: 1vh;
+    }
+    button {
+        border-radius: 10px;
+        background-color: #e0e0e0;
+        width: 2.5vw;
     }
 </style>
